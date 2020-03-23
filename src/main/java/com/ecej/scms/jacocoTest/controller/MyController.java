9606b3d43f0aa00becac181316cf2304384079a3
renwb <believe_wb@163.com>
2020-03-23 13:29:47 +0800

    myController

---
+++ src/main/java/com/ecej/scms/jacocoTest/controller/MyController.java
@@ -0,0 +1,17 @@
+package com.ecej.scms.jacocoTest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myController")
+public class MyController {
+
+	@RequestMapping("/myMethod1")
+	@ResponseBody
+	public String myMethod1() {
+		System.out.println("diff version 1");
+		return "myMethod1 is invoked!!";
+	}
+}
